cef0cb94e5e1b3362831f63df20026ed6fa84972
Nathaniel Delos Santos <npdeloss@ucsd.edu>
2018-08-23 13:56:00 -0700

    add gtf to tsv

---
+++ scripts/gtf_to_tsv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from gtfparse import read_gtf
+
+def main(gtf, tsv):
+    gtf = args.gtf
+    tsv = args.tsv
+    df = read_gtf(gtf)
+    df.to_csv(tsv, sep = '\t')
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description = 'Convert a GTF file to a TSV file')
+    parser.add_argument('gtf', type=int, help='Input GTF file')
+    parser.add_argument('tsv', type=int, help='Output TSV file')
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_arguments
+    main(**args)
